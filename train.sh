0d44f8226db5e224bbb4fb7efb967ab59c33275e
Qi Dang <qida0163@dsv.su.se>
2021-11-03 14:45:40 +0100

    minus change

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --config_path configs/fcos/fcos_resnet50_fpn_fashion_pedia.py -b 2 --gpu_num 4 -t 20211102_fcos_resnet50 --linear_lr
+python train.py --config_path configs/fcos/fcos_resnet50_fpn_coco.py -b 2 --gpu_num 4 --linear_lr "$@"
