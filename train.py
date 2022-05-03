a0abf4480e37c881a82d56b2eb18e8391bb2a200
Qi <dangqi213@gmail.com>
2022-05-03 11:05:11 +0200

    minus change

--- train.py
+++ train.py
@@ -1,20 +1,11 @@
-#import _init
 import sys
 import os
 import argparse
-import pickle
-import platform
-import progressbar
+
 import numpy as np
-import datetime
 from torchcore.data.sampler import distributed_sampler_wrapper
 from torchcore.tools import Logger
-import torchvision
-import math
-from torch import nn
-import json
-import tqdm
-import time
+
 from pprint import pprint
 
 from pycocotools.coco import COCO
@@ -56,7 +47,7 @@ from torchcore.dnn.networks.detectors.build import build_detector
 from torchcore.data.datasets.build import build_dataloader
 from torchcore.dnn.trainer.build import build_trainer
 
-from torchcore.dnn.networks.tools.load_from_mmdetection import load_mm_retinanet, load_mm_fcos
+from torchcore.dnn.networks.tools.load_from_mmdetection import load_mm_retinanet, load_mm_fcos, load_mm_general_model
 
 def parse_commandline():
     parser = argparse.ArgumentParser(description="Training the Model")
@@ -88,105 +79,6 @@ def get_absolute_box(human_box, box):
     box[1]+=int(human_box[1])
     return box
 
-#class my_trainer(DistributedTrainer):
-#
-#    def validate_onece(self):
-#        self.validate()
-#
-#    #def validate(self):
-#    #    if isinstance(self._model, DDP):
-#    #        if not self.is_main_process():
-#    #            print('one process resturn')
-#    #            return
-#    #    if self._cfg.torchvision_model:
-#    #        self._validate_torchvision()
-#    #    else:
-#    #        self._validate()
-#
-#    def _validate( self ):
-#        print('start to validate')
-#        #self._model.test_mode = 'second'
-#        #self._model.module.test_mode = 'second'
-#        self._model.eval()
-#        if isinstance(self._model, DDP):
-#            test_model = self._model.module
-#        else:
-#            test_model = self._model
-#
-#        total_time = 0
-#
-#        results = []
-#        with torch.no_grad() :
-#            for idx,(inputs, targets) in enumerate(tqdm.tqdm(self._testset, 'evaluating')):
-#                inputs = self._set_device( inputs )
-#                start = time.time()
-#                output = test_model( inputs)
-#                batch_size = len(output['boxes'])
-#                #for i, im in enumerate(output):
-#                for i in range(batch_size):
-#                    if len(output['boxes'][i]) == 0:
-#                        continue
-#                    # convert to xywh
-#                    output['boxes'][i][:,2] -= output['boxes'][i][:,0]
-#                    output['boxes'][i][:,3] -= output['boxes'][i][:,1]
-#                    for j in range(len(output['boxes'][i])):
-#                        results.append({'image_id':int(targets[i]['image_id']), 
-#                                        'category_id':output['labels'][i][j].cpu().numpy().round(decimals=2).tolist(), 
-#                                        'bbox':output['boxes'][i][j].cpu().numpy().round(decimals=2).tolist(), 
-#                                        'score':output['scores'][i][j].cpu().numpy().round(decimals=2).tolist()})
-#                total_time += time.time()-start
-#        print('total time is {}s'.format(total_time))
-#        if isinstance(self._model, DDP):
-#            model_time = self._model.module.total_time
-#        else:
-#            model_time = self._model.total_time
-#        print('total model time is {}s'.format(model_time))
-#        print('total model time is {}s'.format(sum(model_time.values())))
-#        print('average time per image is {} s'.format(sum(model_time.values())/len(self._testset)))
-#        average_model_time = {k:v/len(self._testset) for k,v in model_time.items()}
-#        print('model average time per image by part is {}'.format(average_model_time))
-#                
-#        result_path = '{}temp_result.json'.format(self._tag)
-#        with open(result_path,'w') as f:
-#            json.dump(results,f)
-#        self.evaluator.evaluate(result_path)
-#
-#    def _validate_torchvision( self ):
-#        print('start to validate')
-#        self._model.eval()
-#
-#        results = []
-#        with torch.no_grad() :
-#            for idx,(inputs, targets) in enumerate(tqdm.tqdm(self._testset, 'evaluating')):
-#                inputs = self._set_device( inputs )
-#                #if self._cfg.torchvision_model:
-#                #    output = self._model( inputs['data'])
-#                #else:
-#                output = self._model.module( inputs)
-#                batch_size = len(output)
-#                #for i, im in enumerate(output):
-#                for i in range(batch_size):
-#                    if len(output[i]['boxes']) == 0:
-#                        continue
-#                    # convert to xywh
-#                    output[i]['boxes'][:,2] -= output[i]['boxes'][:,0]
-#                    output[i]['boxes'][:,3] -= output[i]['boxes'][:,1]
-#                    for j in range(len(output[i]['boxes'])):
-#                        results.append({'image_id':int(targets[i]['image_id']), 
-#                                        'category_id':output[i]['labels'][j].cpu().numpy().round(decimals=2).tolist(), 
-#                                        'bbox':output[i]['boxes'][j].cpu().numpy().round(decimals=2).tolist(), 
-#                                        'score':output[i]['scores'][j].cpu().numpy().round(decimals=2).tolist()})
-#                #output = self._model['net']( inputs, just_embedding=True) # debug
-#                #bench.update( targets, output )
-#                
-#        out_path = '{}_result.json'.format(self._tag)
-#        with open(out_path,'w') as f:
-#            json.dump(results,f)
-#        self.evaluator.evaluate(out_path)
-#        #self.eval_result(out_path, anno_type='bbox', dataset=self._dataset_name)
-#        #self.eval_result(out_path, anno_type='segm', dataset=self._dataset_name)
-
-
 
 def load_checkpoint(model, path, device, to_print=True):
     #checkpoint = torch.load(path)
@@ -204,29 +96,6 @@ def load_checkpoint(model, path, device, to_print=True):
     if to_print:
         print('Chekpoint has been loaded from {}'.format(path))
 
-#def get_model(cfg, torchvision_model=False):
-#    if torchvision_model:
-#        print('use torchvision model')
-#        backbone = torchvision.models.detection.backbone_utils.resnet_fpn_backbone('resnet50', pretrained=True)
-#        #model = torchvision.models.detection.FasterRCNN(backbone, num_classes=cfg.class_num, min_size=cfg.min_size, max_size=cfg.max_size)
-#        model = torchvision.models.detection.MaskRCNN(backbone, num_classes=cfg.class_num, min_size=cfg.min_size, max_size=cfg.max_size)
-#    else:
-#        backbone = torchvision.models.detection.backbone_utils.resnet_fpn_backbone('resnet50', pretrained=True)
-#        heads = {}
-#        #anchor_generator = MyAnchorGenerator(sizes=((32,),(64,),(128,),(256,),(512,)), aspect_ratios=(0.5,1.0,2.0))
-#        anchor_generator = MyAnchorGenerator(sizes=((16,),(32,),(64,),(128,),(256,)), aspect_ratios=(0.5,1.0,2.0))
-#        #anchor_generator = MyAnchorGenerator(sizes=((32,),(64,),(128,),(256,)), aspect_ratios=(0.5,1.0,2.0))
-#        num_anchors_per_location = anchor_generator.num_anchors_per_location()
-#        assert all(num_anchors_per_location[0] == item for item in num_anchors_per_location)
-#        #print('num anchors per location',num_anchors)
-#        rpn_head = RPNHead(256, num_anchors=num_anchors_per_location[0])
-#        rpn = MyRegionProposalNetwork(anchor_generator, rpn_head, cfg.rpn)
-#        roi_head = RoINet(cfg.roi_head)
-#        heads['rpn'] = rpn
-#        heads['bbox'] = roi_head
-#        #parts = ['heatmap', 'offset', 'width_height']
-#        model = FasterRCNNFPN(backbone, heads=heads, cfg=cfg, training=True, debug_time=True )
-#    return model
 
 def update_linear_lr(trainer_cfg,lr_cfg, batch_size ):
     accumulation_step = trainer_cfg.accumulation_step
@@ -260,7 +129,7 @@ def run(args) :
     batch_size_per_gpu_per_accumulation = args.batch_size
 
     project_path = os.path.expanduser('~/Vision/data')
-    project_name = 'retinanet'
+    project_name = 'yolox'
     cfg.initialize_project(project_name, project_path, tag=tag)
     extra_init={}
     cfg.merge_args( args )
@@ -326,7 +195,7 @@ def run(args) :
         new_lr = None
         if args.linear_lr:
             new_lr = cfg.trainer.optimizer.lr
-        trainer.resume_training(new_lr=new_lr)
+        trainer.resume_training(path=args.load_model_path,new_lr=new_lr)
     #t = my_trainer( cfg, ddp_model, device, data_loader, testset=test_data_loader, dataset_name=args.dataset, train_sampler=dist_train_sampler, benchmark=None, tag=args.tag,evaluator=evaluator, epoch_based=False, eval_step_interval=10000, save_step_interval=10000, rank=rank )
     if not args.evaluate:
         trainer.train()
@@ -335,10 +204,13 @@ def run(args) :
             trainer.load_checkpoint(args.load_model_path, to_print=True)
         if args.load_from_mm_model:
             #checkpoint= 'retinanet_r50_fpn_1x_coco_20200130-c2398f9e.pth'
-            checkpoint= 'fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_1x_coco-0a0d75a8.pth'
+            #checkpoint= 'fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_1x_coco-0a0d75a8.pth'
+            checkpoint = 'yolox_s_8x8_300e_coco_20211121_095711-4592a793.pth'
             #mm_config = 'mmconfigs/retinanet/retinanet_r50_fpn_1x_coco.py'
-            mm_config = 'mmconfigs/fcos/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_1x_coco.py'
-            load_mm_fcos(checkpoint, mm_config, trainer._model,change_backbone=True)
+            #mm_config = 'mmconfigs/fcos/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_1x_coco.py'
+            mm_config = 'mmconfigs/yolox/yolox_s_8x8_300e_coco.py'
+            load_mm_general_model(trainer._model, mm_config,checkpoint)
+            #load_mm_fcos(checkpoint, mm_config, trainer._model,change_backbone=True)
         trainer.validate()
 
 def cleanup():
